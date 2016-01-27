19d13e3aa25b835dbbf2e8d115fd3c5296796f76
Hariharn Srinath <srinathh@gmail.com>
2016-01-27 17:26:54 +0530

    initial program commit. logs requests only for now

---
+++ loggingproxy.go
@@ -0,0 +1,48 @@
+// Command loggingproxy is a http proxy server witht he ability to log requests and responses
+// flowing through the proxy server for in debugging etc. Since it is in pure Go, it
+// is easy to cross-compile it to almost any modern platform and OS.
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/artyom/autoflags"
+)
+
+type config struct {
+	Addr     string `flag:"addr,the address on which to serve the proxy"`
+	Scheme   string `flag:"scheme,the scheme for the remote server"`
+	Host     string `flag:"host,the host of the remote proxy server"`
+	BasePath string `flag:"basepath,the base path of the remote proxy server"`
+	LogReq   bool   `flag:"logreq,log requests to stdout"`
+	LogResp  bool   `flag:"logresp,log responses to stdout"`
+}
+
+var cfg config
+var proxy *httputil.ReverseProxy
+
+func main() {
+
+	cfg.Addr = ":8080"
+	cfg.Scheme = "http"
+	cfg.Host = "localhost:80"
+	cfg.BasePath = ""
+	cfg.LogReq = true
+	cfg.LogResp = false
+
+	autoflags.Define(&cfg)
+	flag.Parse()
+	proxy = httputil.NewSingleHostReverseProxy(&url.URL{Scheme: cfg.Scheme, Host: cfg.Host, Path: cfg.BasePath})
+	http.ListenAndServe(cfg.Addr, http.HandlerFunc(logServe))
+}
+
+func logServe(w http.ResponseWriter, r *http.Request) {
+	if cfg.LogReq {
+		log.Printf("%s : %s", r.RemoteAddr, r.URL.String())
+	}
+	proxy.ServeHTTP(w, r)
+}
